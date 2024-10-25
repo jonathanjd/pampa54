383c86d919094ff3ebc66d58c99e88d32d71b90d
Josiele Da Silva <josiele@outlook.com>
2024-10-25 18:04:20 -0300

    update pageAbout.php

--- wp-content/themes/pampa54/pageAbout.php
+++ wp-content/themes/pampa54/pageAbout.php
@@ -12,6 +12,7 @@
         </section>
 
         <section class="about-three section-space" id="about">
+            
             <div class="container">
                 <div class="row gutter-y-60">
                     <div class="col-lg-4 wow fadeInLeft" data-wow-duration="1500ms">
@@ -23,17 +24,56 @@
 
                                 <h6 class="sec-title__tagline">ABOUT PAMPA54</h6>
 
-                                <h2 class="sec-title__title">ABOUT US TITLE</h2>
+                                <h2 class="sec-title__title">A LEGACY OF QUALITY AND TRADITION</h2>
                             </div>
-                            <p class="about-three__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in sem a massa porta ultricies ac vitae tortor. Phasellus eu mattis urna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam accumsan nibh vitae orci rutrum, imperdiet suscipit nisi semper. Nunc malesuada mauris ut eros eleifend, eget semper quam lobortis. Nulla commodo nisi ac vehicula pulvinar. Nunc at lacinia nibh, ut suscipit libero. Pellentesque risus augue, iaculis tristique nunc luctus, interdum venenatis tortor. Etiam non pulvinar orci, et condimentum nulla. Fusce malesuada facilisis lorem, sed dignissim felis. Maecenas auctor ullamcorper cursus. Quisque id turpis quis velit placerat molestie sed sed risus. Proin accumsan libero quam. Proin ultrices ex quis felis sollicitudin finibus. </p>
-                            
-                        </div>
-                    </div>
-					<div class="col-lg-12 wow fadeInRight" data-wow-duration="1500ms" style="margin-top: 0px;">
-                        <p class="about-three__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in sem a massa porta ultricies ac vitae tortor. Phasellus eu mattis urna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam accumsan nibh vitae orci rutrum, imperdiet suscipit nisi semper. Nunc malesuada mauris ut eros eleifend, eget semper quam lobortis. Nulla commodo nisi ac vehicula pulvinar. Nunc at lacinia nibh, ut suscipit libero. Pellentesque risus augue, iaculis tristique nunc luctus, interdum venenatis tortor. Etiam non pulvinar orci, et condimentum nulla. Fusce malesuada facilisis lorem, sed dignissim felis. Maecenas auctor ullamcorper cursus. Quisque id turpis quis velit placerat molestie sed sed risus. Proin accumsan libero quam. Proin ultrices ex quis felis sollicitudin finibus. </p>
+                            <h4 class="about-three__text">A premium meat distribution company, located in Miami, Fl, US. </h4>
+                        
+                        <p class="about-three__text">PAMPA54 is a company founded by a visionary entrepreneur driven by passion, integrity, and a deep-rooted connection to the world of meat. </p>
                     </div>
                 </div>
             </div>
+</SECTION>
+<SECTION CLASS="CONTAINER-SERVICES">
+    <DIV class="container" style="text-align: center;">
+<h2 class="sec-title__title">OUR SERVICES</h2>
+</DIV>
+            <div class="container mt-5">
+            
+        <div class="row">
+            <div class="col-md-6 col-lg-6 mb-4">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h5 class="sec-title__tagline">Authentic Flavor</h5>
+                        <p class="card-text">Enjoy the genuine taste of Argentinean chorizos, crafted with traditional recipes and high-quality ingredients.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-6 mb-4">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h5 class="sec-title__tagline">Consistent Excellence</h5>
+                        <p class="card-text">With over 30 years of expertise and Argentinean roots, PAMPA54 guarantees consistent quality and flavor in every product.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-6 mb-4">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h5 class="sec-title__tagline">Premium Quality</h5>
+                        <p class="card-text">Our sausages are made from carefully selected, natural ingredients, ensuring superior taste and texture. Our sausages provide a wholesome and nutritious option without compromising on taste.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-6 mb-4">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h5 class="sec-title__tagline">Customer Satisfaction</h5>
+                        <p class="card-text">Dedicated to delivering an unparalleled gastronomic experience, ensuring our customers' satisfaction with every purchase.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
         </section>
 
 <?php get_footer(); ?>
