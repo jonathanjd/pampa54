8b62f14484d70dfa48a0a7ca8d0971b73ff2e523
Albert18 <albert.cowore18@gmail.com>
2025-01-09 17:52:09 -0500

    Ajuste carrusel about

--- wp-content/themes/pampa54/pageAbout.php
+++ wp-content/themes/pampa54/pageAbout.php
@@ -24,7 +24,34 @@
             </div>
 </section>
 
-<section class="gallery-instagram">
+        <hr>
+        <section>
+            <div class="sec-title sec-title--center">
+                <h6 class="sec-title__tagline">Our Company / Pampa54</h6>
+                <h2 class="sec-title__title" style=" font-size:80px; ">Our  Mission</h2>
+            </div>
+       
+            <div class="container">
+                <div class="row gutter-y-60">
+                    <div class="col-lg-4 wow fadeInLeft" data-wow-duration="1500ms">
+                    <img src="<?php echo get_template_directory_uri();?>/assets/images/about/about-3-4.png" alt="" class="about-three__image__two">
+                        
+                    </div>
+                    <div class="col-lg-8 wow fadeInRight" data-wow-duration="1500ms">
+                        <div class="about-three__content">
+                            <p class="about-three__text" style="text-align: justify;">At <b>PAMPA54</b>, our mission is to bring the authentic taste of Argentina’s finest beef and Argentinean Gourmet chorizos to the United States. </p>         
+                        <p>Founded by a passionate and visionary entrepreneur, <b>PAMPA54</b> is driven by a commitment to integrity and a deep respect for the art of fine meats and rooted in a legacy of quality, tradition, and excellence. We provide only the highest quality products.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <br>
+        <br>
+        <br>
+        <br>
+
+        <section class="gallery-instagram">
             <div class="container-fluid">
                 <div class="gallery-instagram__grid">
                     <div class="gallery-instagram__item wow fadeInUp" data-wow-duration="1500ms" data-wow-delay="00ms">
@@ -74,26 +101,5 @@
             
         </section>
 
-        <section class="about-three section-space" id="about">
-            <div class="sec-title sec-title--center">
-                <h6 class="sec-title__tagline">Our Company / Pampa54</h6>
-                <h2 class="sec-title__title" style=" font-size:80px; ">Our  Mission</h2>
-            </div>
-       
-            <div class="container">
-                <div class="row gutter-y-60">
-                    <div class="col-lg-4 wow fadeInLeft" data-wow-duration="1500ms">
-                    <img src="<?php echo get_template_directory_uri();?>/assets/images/about/about-3-4.png" alt="" class="about-three__image__two">
-                        
-                    </div>
-                    <div class="col-lg-8 wow fadeInRight" data-wow-duration="1500ms">
-                        <div class="about-three__content">
-                            <p class="about-three__text" style="text-align: justify;">At <b>PAMPA54</b>, our mission is to bring the authentic taste of Argentina’s finest beef and Argentinean Gourmet chorizos to the United States. </p>         
-                        <p>Founded by a passionate and visionary entrepreneur, <b>PAMPA54</b> is driven by a commitment to integrity and a deep respect for the art of fine meats and rooted in a legacy of quality, tradition, and excellence. We provide only the highest quality products.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
 
 <?php get_footer(); ?>
